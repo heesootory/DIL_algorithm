b8db1edf01af37034e3b15e12bdf8dabf322c66d
heesootory <93choiheesoo@gmail.com>
2022-06-24 00:35:19 +0900

    Update 폭죽쇼.cs

--- 기초-브론즈 문제/백준_1773_폭죽쇼/폭죽쇼.cs
+++ 기초-브론즈 문제/백준_1773_폭죽쇼/폭죽쇼.cs
@@ -1,3 +1,34 @@
+using System;
+using static System.Console;
 
+namespace cs_algo
+{
+    class Program
+    {
+        public static void  Main(string[] args)
+        {
+            int n, c;
+            int[] time = new int[101];
+            bool[] fire = new bool[2000001];
+            string[] s = ReadLine().Split(' ');
+            n = int.Parse(s[0]);
+            c = int.Parse(s[1]);
+            for (int i = 0; i < n; i++)
+                time[i] = int.Parse(ReadLine());
+
+            int cnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = time[i]; j <= c; j += time[i])
+                {
+                    if (fire[j]) continue;
+                    fire[j] = true;
+                    cnt++;
+                }
+            }
+            WriteLine(cnt);
+        }
+    }
+}
 
 
