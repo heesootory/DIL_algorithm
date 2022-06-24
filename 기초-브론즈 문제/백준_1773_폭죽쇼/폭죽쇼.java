beb35812e6ffde3498a53ff93799b61ceb829fc0
heesootory <93choiheesoo@gmail.com>
2022-06-24 14:53:17 +0900

    Update 폭죽쇼.java

--- 기초-브론즈 문제/백준_1773_폭죽쇼/폭죽쇼.java
+++ 기초-브론즈 문제/백준_1773_폭죽쇼/폭죽쇼.java
@@ -1,3 +1,23 @@
+import java.util.*;
 
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n,c;
+        n = s.nextInt();
+        c = s.nextInt();
+        int[] time = new int[n];
+        boolean[] fire = new boolean[2000001];
+        for(int i = 0; i< n; i++) time[i] = s.nextInt();
 
-
+        int cnt = 0;
+        for(int i = 0; i< n; i++){
+            for(int j = time[i]; j <= c; j +=time[i]){
+                if(fire[j]) continue;
+                fire[j] = true;
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
