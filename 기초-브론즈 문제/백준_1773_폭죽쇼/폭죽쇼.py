5789d7e24083be1f7d0637f946266cf7b004cd49
heesootory <93choiheesoo@gmail.com>
2022-06-24 16:00:43 +0900

    Update 폭죽쇼.py

--- 기초-브론즈 문제/백준_1773_폭죽쇼/폭죽쇼.py
+++ 기초-브론즈 문제/백준_1773_폭죽쇼/폭죽쇼.py
@@ -1,3 +1,18 @@
+n,c = map(int, input().split())
+time = []
+fire = [0 for i in range(c+1)]
+for i in range(n):
+    time.append(int(input()))
+
+cnt = 0
+for i in range(n):
+    for j in range(time[i], c+1, time[i]):
+        if fire[j] == 1: continue
+        fire[j] = 1
+        cnt+=1
+
+print(cnt)
+
 
 
 
