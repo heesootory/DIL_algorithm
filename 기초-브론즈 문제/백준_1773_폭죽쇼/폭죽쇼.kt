3ade0f292e2c6d4276a3db6f48f6dd3ade71e21b
heesootory <93choiheesoo@gmail.com>
2022-06-24 14:58:46 +0900

    Update 폭죽쇼.kt

--- 기초-브론즈 문제/백준_1773_폭죽쇼/폭죽쇼.kt
+++ 기초-브론즈 문제/백준_1773_폭죽쇼/폭죽쇼.kt
@@ -1,3 +1,24 @@
+import java.util.*
 
+fun main(args: Array<String>) {
+    val s = Scanner(System.`in`)
+    val n = s.nextInt()
+    val c = s.nextInt()
+    var fire = BooleanArray(2000001, {false})
+    var time = IntArray(n, {0})
+    for(i in 0..n-1)
+        time[i] = s.nextInt()
+
+    var cnt = 0
+    for(i in 0..n-1){
+        for(j in time[i]..c step time[i]){
+            if(fire[j]) continue
+            fire[j] = true
+            cnt++
+        }
+    }
+    print("${cnt}")
+
+}
 
 
