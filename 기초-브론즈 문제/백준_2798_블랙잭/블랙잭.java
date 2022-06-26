db3fc4defc97da32ffb50726cb0b8731698684a7
heesootory <93choiheesoo@gmail.com>
2022-06-26 20:11:15 +0900

    Create 블랙잭.java

---
+++ 기초-브론즈 문제/백준_2798_블랙잭/블랙잭.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int m = s.nextInt();
+        int[] arr = new int[100];
+        for(int i = 0; i < n; i++){
+            arr[i] = s.nextInt();
+        }
+
+        int max = 0;
+        for(int i = 0; i< n-2; i++){
+            for(int j = i+1; j< n-1; j++){
+                for(int k = j+1; k < n; k++){
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if(sum <= m){
+                        if(sum > max) max = sum;
+                    }
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
