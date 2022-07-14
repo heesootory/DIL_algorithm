4a93e6e7aaabf2374c2a537c2da85651f1834267
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2022-07-14 17:32:27 +0900

    Create 1859.백만 장자 프로젝트.java

---
+++ SW_ssafy_expert/D2/1859.백만 장자 프로젝트.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class 일대일가위바위보 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int t = s.nextInt();
+		int _case = 1;
+		while(t-- > 0) {
+			int n = s.nextInt();
+			int[] arr = new int[n];
+			for(int i = 0; i< arr.length; i++) arr[i] = s.nextInt();
+			long sum = 0;
+			int big = arr[n-1];
+			for(int i = n-2; i>=0; i--) {
+				if(arr[i] > big) big = arr[i];
+				else sum += (big - arr[i]);
+			}
+			System.out.println("#"+ _case + " " + sum);
+			_case++;
+		}
+
+	}
+}
