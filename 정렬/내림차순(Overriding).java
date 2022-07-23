06101633aedd088a3a20cc423fdf309adafb584f
heesootory <93choiheesoo@gmail.com>
2022-07-23 19:13:40 +0900

    7_23 정렬 수정

--- 정렬/내림차순(Overriding).java
+++ 정렬/내림차순(Overriding).java
@@ -3,6 +3,11 @@ import java.util.*;
 /**
  *  객체의 정렬 기준을 정의하는 방법으로 정렬 대상 클래스를 자바에서 기본적으로 제공하고 있는 Comparable 인터페이스를 구현함.
  *  이 인터페이스 안에 있는 compareTo() 메서드를 통해 인자로 넘어온 같은 타입의 객체를 비교가능.
+ *  sort함수, PriorityQueue, Collections 등 모든 정렬 배열의 함수들이 이 인턴페이스의 비교 함수를 가지고 구현되므로, 
+ *  이 함수를 수정함으로써 쉽게 정렬을 커스터마이징 할 수 있다.
+ * 
+ *  tip) 매개변수로 받은 o의 값이 현재 값보다 뒤에 있는 값일때, 
+ *       return 값이 항상 양수가 되도록 배열한다고 생각하자.
  */
 
 class Pair implements Comparable<Pair>{
