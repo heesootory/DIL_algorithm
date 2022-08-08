57d0d3e568c1f546ea37d791180f548b8554e8f6
heesootory <93choiheesoo@gmail.com>
2022-08-09 00:09:00 +0900

    Create 백준_11279_최대힙.java

---
+++ 자료구조/백준_11279_최대힙.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+//        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());      // 동일한 방법.
+
+        int n = Integer.parseInt(st.nextToken());
+
+        while(n-- > 0){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+
+            if(x == 0){
+                if(pq.size() == 0) System.out.println(0);
+                else System.out.println(pq.poll());
+            }else{
+                pq.add(x);
+            }
+        }
+
+    }
+}
+
